c4eaf10c8202dcd3e7997e91fc2a4a774cedc3d7
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2022-12-16 04:42:01 +0545

    added pull to refresh

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -17,104 +17,109 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     WorldStats worldStats = WorldStats();
     var controller = Get.put(HomeScreenController());
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.05,
-          ),
-          const Text(
-            "Hello, Rj Lama",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.02,
-          ),
-          const Text(
-            "Here what's cooking for you",
-            style: TextStyle(fontSize: 16, color: Colors.black45),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.02,
-          ),
-          FutureBuilder(
-            future: worldStats.fetchData(),
-            builder: (context, AsyncSnapshot<WorldStatsModel> snapshot) {
-              if (snapshot.hasData) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    PieChart(
-                        dataMap: {
-                          "total": double.parse(
-                              snapshot.data!.todayCases.toString()),
-                          "recovered": double.parse(
-                              snapshot.data!.todayRecovered.toString()),
-                          "death": double.parse(
-                              snapshot.data!.todayDeaths.toString()),
-                        },
-                        colorList: controller.pieChartColors,
-                        chartType: ChartType.ring,
-                        chartRadius: MediaQuery.of(context).size.height * 0.15),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.03,
-                    ),
-                    const Text(
-                      "Details",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.02,
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.75,
-                      child: GridView(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2),
-                        children: [
-                          Grids(
-                              color: box1.withOpacity(0.3),
-                              info: snapshot.data!.todayCases.toString(),
-                              title: "Total\nCases"),
-                          Grids(
-                              color: box2.withOpacity(0.2),
-                              info: snapshot.data!.todayRecovered.toString(),
-                              title: "Todays\nRecovered"),
-                          Grids(
-                              color: box3.withOpacity(0.2),
-                              info: snapshot.data!.todayDeaths.toString(),
-                              title: "Todays\nDeths"),
-                          Grids(
-                              color: box4.withOpacity(0.3),
-                              info: snapshot.data!.affectedCountries.toString(),
-                              title: "Affected\nCountries"),
-                          Grids(
-                              color: box4.withOpacity(0.3),
-                              info: snapshot.data!.active.toString(),
-                              title: "Active\nCases"),
-                          Grids(
-                              color: box4.withOpacity(0.3),
-                              info: snapshot.data!.critical.toString(),
-                              title: "Critical\nCases"),
-                        ],
+    return RefreshIndicator(
+      onRefresh: () => worldStats.fetchData(),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            const Text(
+              "Hello, Rj Lama",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            const Text(
+              "Here what's cooking for you",
+              style: TextStyle(fontSize: 16, color: Colors.black45),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            FutureBuilder(
+              future: worldStats.fetchData(),
+              builder: (context, AsyncSnapshot<WorldStatsModel> snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      PieChart(
+                          dataMap: {
+                            "total": double.parse(
+                                snapshot.data!.todayCases.toString()),
+                            "recovered": double.parse(
+                                snapshot.data!.todayRecovered.toString()),
+                            "death": double.parse(
+                                snapshot.data!.todayDeaths.toString()),
+                          },
+                          colorList: controller.pieChartColors,
+                          chartType: ChartType.ring,
+                          chartRadius:
+                              MediaQuery.of(context).size.height * 0.15),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.03,
                       ),
-                    ),
-                  ],
-                );
-              } else {
-                return Center(
-                  child: Lottie.asset("assets/images/progessindicator.json",
-                      height: MediaQuery.of(context).size.height * 0.3),
-                );
-              }
-            },
-          ),
-        ],
+                      const Text(
+                        "Details",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.02,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.75,
+                        child: GridView(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2),
+                          children: [
+                            Grids(
+                                color: box1.withOpacity(0.3),
+                                info: snapshot.data!.todayCases.toString(),
+                                title: "Total\nCases"),
+                            Grids(
+                                color: box2.withOpacity(0.2),
+                                info: snapshot.data!.todayRecovered.toString(),
+                                title: "Todays\nRecovered"),
+                            Grids(
+                                color: box3.withOpacity(0.2),
+                                info: snapshot.data!.todayDeaths.toString(),
+                                title: "Todays\nDeths"),
+                            Grids(
+                                color: box4.withOpacity(0.3),
+                                info:
+                                    snapshot.data!.affectedCountries.toString(),
+                                title: "Affected\nCountries"),
+                            Grids(
+                                color: box4.withOpacity(0.3),
+                                info: snapshot.data!.active.toString(),
+                                title: "Active\nCases"),
+                            Grids(
+                                color: box4.withOpacity(0.3),
+                                info: snapshot.data!.critical.toString(),
+                                title: "Critical\nCases"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                } else {
+                  return Center(
+                    child: Lottie.asset("assets/images/progessindicator.json",
+                        height: MediaQuery.of(context).size.height * 0.3),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
