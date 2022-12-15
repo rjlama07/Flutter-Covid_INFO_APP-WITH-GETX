1f885e5a262302da349fdddd128b209a74774456
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2022-12-16 01:23:38 +0545

    feat: opens onbording only for the first time
    added dependency cached network image

--- lib/controller/splash_screen_controller.dart
+++ lib/controller/splash_screen_controller.dart
@@ -1,13 +1,25 @@
 import 'dart:async';
-
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreenController extends GetxController {
   @override
   void onInit() {
     super.onInit();
     Timer(const Duration(seconds: 2), () {
-      Get.offNamed('/onBoarding');
+      nextScreen();
     });
+  }
+
+  Future nextScreen() async {
+    bool? newState;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    newState = prefs.getBool("newState");
+    await prefs.setBool("newState", true);
+    if (newState == null || !newState) {
+      Get.offAndToNamed('/onBoarding');
+    } else {
+      Get.offAndToNamed('/bottomNavBar');
+    }
   }
 }
