b8c05dfef0c159761781522866a1aa4d3547206d
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2022-12-19 23:01:02 +0545

    set fixed device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,19 @@ import 'package:coivd_19_app/screens/splash_screen.dart';
 import 'package:coivd_19_app/widgets/bottomNavBar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
